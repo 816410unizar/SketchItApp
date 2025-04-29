d723069a769b84e9b76ffa825fc883cbba953493
Javier Ferreras Pajarin <148547228+816410unizar@users.noreply.github.com>
2025-04-29 14:27:09 +1000

    Initial Commit

---
+++ SketchItApp/SketchItAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchItAppApp.swift
+//  SketchItApp
+//
+//  Created by Javier Ferreras Pajarin on 29/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct SketchItAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
